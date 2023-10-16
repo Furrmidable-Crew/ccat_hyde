1b29a71ac7c321f59124f9e187ab836103f8a991
Nicola <nicorb932@hotmail.com>
2023-10-16 23:03:54 +0200

    add hyde prompt in settings

--- hyde.py
+++ hyde.py
@@ -1,33 +1,24 @@
+import json
 import langchain
-
 from cat.mad_hatter.decorators import hook
 from cat.log import log
 
 
+with open("cat/plugins/ccat_hyde/settings.json", "r") as json_file:
+    settings = json.load(json_file)
+
+
 @hook(priority=1)
 def cat_recall_query(user_message, cat):
-    # Prompt
-    hyde_prompt = """You will be given a sentence.
-    If the sentence is a question, convert it to a plausible answer. If the sentence does not contain a question, 
-    just repeat the sentence as is without adding anything to it.
-
-    Examples:
-    - what furniture there is in my room? --> In my room there is a bed, a wardrobe and a desk with my computer
-    - where did you go today --> today I was at school
-    - I like ice cream --> I like ice cream
-    - how old is Jack --> Jack is 20 years old
-
-    Sentence:
-    - {input} --> """  # TODO make this a setting
 
     # Make a prompt from template
     hypothesis_prompt = langchain.PromptTemplate(
         input_variables=["input"],
-        template=hyde_prompt
+        template=settings["hyde_prompt"]
     )
 
     # Run a LLM chain with the user message as input
     hypothesis_chain = langchain.chains.LLMChain(prompt=hypothesis_prompt, llm=cat._llm)
     answer = hypothesis_chain(user_message)
-    log(answer, "ERROR")
+    log(answer, "INFO")
     return answer["text"]
