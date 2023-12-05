ef40b17160a7d68f92d2212bde1cd37860f13808
D'Amico Massimiliano <Massimiliano.DAmico@exprivia.com>
2023-12-05 16:47:53 +0100

    update average vectors

--- hyde.py
+++ hyde.py
@@ -24,25 +24,26 @@ def cat_recall_query(user_message, cat):
     log(answer, "INFO")
     
     # Calculate hyde embedding
-    cat.working_memory["hyde_embedding"] = cat.embedder(answer["text"])
+    cat.working_memory["hyde_embedding"] = cat.embedder.embed_query(answer["text"])
     return user_message
 
 
 # Calculates the average between the user's message embedding and the Hyde response embedding
-def _calculate_vector_average(config, cat):
-    user_embedding = config.embedding
+def _calculate_vector_average(config: dict, cat):
+    user_embedding = config['embedding']
     hyde_embedding = cat.working_memory["hyde_embedding"]
-    config.embedding = (user_embedding + hyde_embedding) / 2
+    average_embedding = [(x + y)/2 for x, y in zip(user_embedding, hyde_embedding)]
+    config['embedding'] = average_embedding
 
 
 @hook(priority=1)
-def before_cat_recalls_episodic_memories(config, cat):
+def before_cat_recalls_episodic_memories(config: dict, cat):
     _calculate_vector_average(config, cat)
 
 @hook(priority=1)
-def before_cat_recalls_declarative_memories(config, cat):
+def before_cat_recalls_declarative_memories(config: dict, cat):
     _calculate_vector_average(config, cat)
 
 @hook(priority=1)
-def before_cat_recalls_procedural_memories(config, cat):
+def before_cat_recalls_procedural_memories(config: dict, cat):
     _calculate_vector_average(config, cat)
